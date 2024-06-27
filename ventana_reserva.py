f7dd633ea56d684202deb0ceb9b166dea75886bc
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-06-27 19:56:27 -0400

    problema solucionado
    utilizo ia para cambiar rapido vvariables dentro de todo el codigo

--- ventana_reserva.py
+++ ventana_reserva.py
@@ -19,45 +19,51 @@ class VentanaReserva:
         self.entry_id_habitacion = tk.Entry(master)
         self.entry_id_habitacion.grid(row=1, column=1)
         
-        self.label_id_cliente = tk.Label(master, text="ID de Cliente:")
-        self.label_id_cliente.grid(row=2, column=0)
-        self.entry_id_cliente = tk.Entry(master)
-        self.entry_id_cliente.grid(row=2, column=1)
+        self.label_id_usuario = tk.Label(master, text="ID de Usuario:")
+        self.label_id_usuario.grid(row=2, column=0)
+        self.entry_id_usuario = tk.Entry(master)
+        self.entry_id_usuario.grid(row=2, column=1)
         
-        self.label_fecha_entrada = tk.Label(master, text="Fecha de Entrada:")
-        self.label_fecha_entrada.grid(row=3, column=0)
-        self.entry_fecha_entrada = tk.Entry(master)
-        self.entry_fecha_entrada.grid(row=3, column=1)
+        self.label_fecha_llegada = tk.Label(master, text="Fecha de Llegada:")
+        self.label_fecha_llegada.grid(row=3, column=0)
+        self.entry_fecha_llegada = tk.Entry(master)
+        self.entry_fecha_llegada.grid(row=3, column=1)
         
         self.label_fecha_salida = tk.Label(master, text="Fecha de Salida:")
         self.label_fecha_salida.grid(row=4, column=0)
         self.entry_fecha_salida = tk.Entry(master)
         self.entry_fecha_salida.grid(row=4, column=1)
         
-        self.label_estado = tk.Label(master, text="Estado:")
-        self.label_estado.grid(row=5, column=0)
-        self.entry_estado = tk.Entry(master)
-        self.entry_estado.grid(row=5, column=1)
+        self.label_estado_reserva = tk.Label(master, text="Estado de la Reserva:")
+        self.label_estado_reserva.grid(row=5, column=0)
+        self.entry_estado_reserva = tk.Entry(master)
+        self.entry_estado_reserva.grid(row=5, column=1)
+        
+        self.label_precio_total = tk.Label(master, text="Precio Total:")
+        self.label_precio_total.grid(row=6, column=0)
+        self.entry_precio_total = tk.Entry(master)
+        self.entry_precio_total.grid(row=6, column=1)
         
         # Botones de acciones
         self.button_agregar = tk.Button(master, text="Agregar", command=self.agregar_reserva)
-        self.button_agregar.grid(row=6, column=0)
+        self.button_agregar.grid(row=7, column=0)
         
         self.button_modificar = tk.Button(master, text="Modificar", command=self.modificar_reserva)
-        self.button_modificar.grid(row=6, column=1)
+        self.button_modificar.grid(row=7, column=1)
         
         self.button_eliminar = tk.Button(master, text="Eliminar", command=self.eliminar_reserva)
-        self.button_eliminar.grid(row=6, column=2)
+        self.button_eliminar.grid(row=7, column=2)
         
         # Tabla para mostrar las reservas
-        self.tree = ttk.Treeview(master, columns=("ID", "ID de Habitacion", "ID de Cliente", "Fecha de Entrada", "Fecha de Salida", "Estado"), show="headings")
+        self.tree = ttk.Treeview(master, columns=("ID", "ID de Habitacion", "ID de Usuario", "Fecha de Llegada", "Fecha de Salida", "Precio Total", "Estado de la Reserva"), show="headings")
         self.tree.heading("ID", text="ID")
         self.tree.heading("ID de Habitacion", text="ID de Habitacion")
-        self.tree.heading("ID de Cliente", text="ID de Cliente")
-        self.tree.heading("Fecha de Entrada", text="Fecha de Entrada")
+        self.tree.heading("ID de Usuario", text="ID de Usuario")
+        self.tree.heading("Fecha de Llegada", text="Fecha de Llegada")
         self.tree.heading("Fecha de Salida", text="Fecha de Salida")
-        self.tree.heading("Estado", text="Estado")
-        self.tree.grid(row=7, column=0, columnspan=3)
+        self.tree.heading("Precio Total", text="Precio Total")
+        self.tree.heading("Estado de la Reserva", text="Estado de la Reserva")
+        self.tree.grid(row=8, column=0, columnspan=3)
         
         # Cargar las reservas existentes
         self.cargar_reservas()
@@ -65,32 +71,33 @@ class VentanaReserva:
     def cargar_reservas(self):
         db = Database()
         query = """
-        SELECT r.id_reserva, dr.id_habitacion, r.id_cliente, r.fecha_entrada, r.fecha_salida, r.estado
+        SELECT r.id_reserva, dr.id_habitacion, r.id_usuario, r.fecha_llegada, r.fecha_salida, r.precio_total, r.id_estado_reserva
         FROM reserva r
         JOIN detalle_reserva dr ON r.id_reserva = dr.id_reserva
         """
         rows = db.fetch_all(query)
         for row in rows:
-            self.tree.insert("", "end", values=(row['id_reserva'], row['id_habitacion'], row['id_cliente'], row['fecha_entrada'], row['fecha_salida'], row['estado']))
-            
+            self.tree.insert("", "end", values=(row['id_reserva'], row['id_habitacion'], row['id_usuario'], row['fecha_llegada'], row['fecha_salida'], row['precio_total'], row['id_estado_reserva']))
+    
     def agregar_reserva(self):
         id_reserva = self.entry_id_reserva.get()
         id_habitacion = self.entry_id_habitacion.get()
-        id_cliente = self.entry_id_cliente.get()
-        fecha_entrada = self.entry_fecha_entrada.get()
+        id_usuario = self.entry_id_usuario.get()
+        fecha_llegada = self.entry_fecha_llegada.get()
         fecha_salida = self.entry_fecha_salida.get()
-        estado = self.entry_estado.get()
+        precio_total = self.entry_precio_total.get()
+        id_estado_reserva = self.entry_estado_reserva.get()
         
-        if not all([id_reserva, id_habitacion, id_cliente, fecha_entrada, fecha_salida, estado]):
+        if not all([id_reserva, id_habitacion, id_usuario, fecha_llegada, fecha_salida, precio_total, id_estado_reserva]):
             messagebox.showerror("Error", "Todos los campos son obligatorios")
             return
         
         db = Database()
         try:
             db.execute_query("""
-            INSERT INTO reserva (id_reserva, id_cliente, fecha_entrada, fecha_salida, estado)
-            VALUES (%s, %s, %s, %s, %s)
-            """, (id_reserva, id_cliente, fecha_entrada, fecha_salida, estado))
+            INSERT INTO reserva (id_reserva, id_usuario, fecha_llegada, fecha_salida, precio_total, id_estado_reserva)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, (id_reserva, id_usuario, fecha_llegada, fecha_salida, precio_total, id_estado_reserva))
             
             db.execute_query("""
             INSERT INTO detalle_reserva (id_reserva, id_habitacion)
@@ -111,12 +118,13 @@ class VentanaReserva:
         
         id_reserva = self.entry_id_reserva.get()
         id_habitacion = self.entry_id_habitacion.get()
-        id_cliente = self.entry_id_cliente.get()
-        fecha_entrada = self.entry_fecha_entrada.get()
+        id_usuario = self.entry_id_usuario.get()
+        fecha_llegada = self.entry_fecha_llegada.get()
         fecha_salida = self.entry_fecha_salida.get()
-        estado = self.entry_estado.get()
+        precio_total = self.entry_precio_total.get()
+        id_estado_reserva = self.entry_estado_reserva.get()
         
-        if not all([id_reserva, id_habitacion, id_cliente, fecha_entrada, fecha_salida, estado]):
+        if not all([id_reserva, id_habitacion, id_usuario, fecha_llegada, fecha_salida, precio_total, id_estado_reserva]):
             messagebox.showerror("Error", "Todos los campos son obligatorios")
             return
         
@@ -124,9 +132,9 @@ class VentanaReserva:
         try:
             db.execute_query("""
             UPDATE reserva
-            SET id_cliente = %s, fecha_entrada = %s, fecha_salida = %s, estado = %s
+            SET id_usuario = %s, fecha_llegada = %s, fecha_salida = %s, precio_total = %s, id_estado_reserva = %s
             WHERE id_reserva = %s
-            """, (id_cliente, fecha_entrada, fecha_salida, estado, id_reserva))
+            """, (id_usuario, fecha_llegada, fecha_salida, precio_total, id_estado_reserva, id_reserva))
             
             db.execute_query("""
             UPDATE detalle_reserva
@@ -158,10 +166,3 @@ class VentanaReserva:
             self.cargar_reservas()  # Recargar reservas
         except Exception as e:
             messagebox.showerror("Error", str(e))
-            
-            
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("100x800")  # Ancho de 800 píxeles y alto de 600 píxeles
-    app = VentanaReserva(root)
-    root.mainloop()
