a73b580762c7849a5ac4dbefab0661a55f38d838
FlakoBIG <Collado_81@hotmail.cl>
2024-07-07 16:14:25 -0400

    Creando reserva

--- Reservar/database.py
+++ Reservar/database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
+from datetime import date
 
 class Database:
     def __init__(self):
@@ -26,6 +27,50 @@ class Database:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
 
+    def insert_reserva(self, entrada, salida, id_usuario):
+        # Determinar el estado de la reserva
+        today = date.today()
+        if entrada == today:
+            estado_reserva = "Activa"
+        elif entrada > today:
+            estado_reserva = "Reservada"
+        else:
+            estado_reserva = "Terminada"
+
+        # Insertar en la tabla estado_reserva
+        query_estado_reserva = """
+        INSERT INTO estado_reserva (estado)
+        VALUES (%s)
+        """
+        self.cursor.execute(query_estado_reserva, (estado_reserva,))
+        id_estado_reserva = self.cursor.lastrowid
+
+        # Insertar en la tabla reserva
+        query_reserva = """
+        INSERT INTO reserva (fecha_llegada, fecha_salida, id_usuario, id_estado_reserva)
+        VALUES (%s, %s, %s, %s)
+        """
+        self.cursor.execute(query_reserva, (entrada, salida, id_usuario, id_estado_reserva))
+        id_reserva = self.cursor.lastrowid
+
+        self.conn.commit()
+        return id_reserva
+
+    def insert_detalle_reserva(self, id_reserva, id_habitacion):
+        query = """
+        INSERT INTO detalle_reserva (id_reserva, id_habitacion)
+        VALUES (%s, %s)
+        """
+        self.cursor.execute(query, (id_reserva, id_habitacion))
+        self.conn.commit()
+
+    def insert_detalle_huesped(self, id_reserva, id_huespedes_h, id_responsable):
+        query = """
+        INSERT INTO detalle_huespedes (id_reserva, id_huespedes_h, id_responsable)
+        VALUES (%s, %s, %s)
+        """
+        self.cursor.execute(query, (id_reserva, id_huespedes_h, id_responsable))
+        self.conn.commit()
 
     def cargar_habitacion_por_id(self, id_habitacion):
         query = f"""
