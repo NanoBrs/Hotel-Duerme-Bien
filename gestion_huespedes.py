7aa7b7763b1cafd66a2ac13a97ad3ef1e1606d0f
Reeddox <oscaracevedosanchez@gmail.com>
2024-07-12 00:07:56 -0400

    InterfazDiseño
    Se intento implementar el diseño de la interfaz

--- gestion_huespedes.py
+++ gestion_huespedes.py
@@ -16,6 +16,19 @@ class GestionHuespedes(tk.Toplevel):
         style = ttk.Style()
         style.theme_use('clam')
         
+        # Agregar imagen de fondo
+        self.background_image = tk.PhotoImage(file="img/MENU2.png")
+        self.background_label = tk.Label(self, image=self.background_image)
+        self.background_label.place(relwidth=1, relheight=1)
+        
+        # Frame para el formulario de ingreso y edición
+        form_frame = ttk.Frame(self, padding=(20, 10))
+        form_frame.place(x=11.4, y=84.6, width=975.8, height=300)  # Ajustado para la mitad superior del área deseada
+
+        # Campos de entrada
+        # Primera columna
+        ttk.Label(form_frame, text="GESTION DE HUESPEDES").place(x=5, y=10)
+        
         # Frame principal
         main_frame = ttk.Frame(self, padding = "10 10 10 10")
         main_frame.pack(fill = tk.BOTH, expand = True)
@@ -50,6 +63,7 @@ class GestionHuespedes(tk.Toplevel):
         ttk.Button(buscar_frame, text = "Buscar", command = self.buscar_huesped).pack(side = tk.LEFT, padx = 5)
         #Boton Restablecer
         ttk.Button(buscar_frame, text="Restablecer", command = self.restablecer_lista).pack(side = tk.LEFT, padx = 5)
+        
         # Lista de huéspedes
         self.tree = ttk.Treeview(main_frame, columns = ["ID"] + self.campos, show = 'headings')
         self.tree.heading("ID", text = "ID")
@@ -62,6 +76,47 @@ class GestionHuespedes(tk.Toplevel):
         self.tree.bind("<Double-1>", self.huesped_select)
         
         self.cargar_datos()
+        
+        #--------------------------------------------------------- MENU -----------------------------------------------------------------
+        # Configuración de estilos
+        style = ttk.Style()
+        style.configure("rounded.TButton", borderwidth=2, relief="solid", background="white", padding=10, font=('Helvetica', 12))
+        style.map("rounded.TButton", background=[('active', 'lightgray')])
+
+        # Botón Gestionar Habitaciones
+        self.boton_gestionar_habitaciones = ttk.Button(self, text="HABITACIONES", command=self.mostrar_gestion_habitaciones, style="rounded.TButton")
+        self.boton_gestionar_habitaciones.place(x=1055, y=266, width=165, height=45)
+
+        # Botón Gestionar Huéspedes
+        self.boton_gestionar_huespedes = ttk.Button(self, text="HUESPEDES", command=self.mostrar_gestion_huespedes, style="rounded.TButton")
+        self.boton_gestionar_huespedes.place(x=1055, y=366, width=165, height=45)
+
+        # Botón Gestionar Reservas
+        self.boton_gestionar_reservas = ttk.Button(self, text="RESERVAS", command=self.mostrar_gestion_reservas, style="rounded.TButton")
+        self.boton_gestionar_reservas.place(x=1055, y=466, width=165, height=45)
+
+        # Botón Cerrar Sesión
+        self.boton_cerrar_sesion = ttk.Button(self, text="CERRAR SESIÓN", command=self.cerrar_sesion, style="rounded.TButton")
+        self.boton_cerrar_sesion.place(x=1055, y=566, width=165, height=45)
+
+
+    def mostrar_gestion_habitaciones(self):
+        self.controlador.mostrar_frame("GestionHabitaciones")
+
+    def mostrar_gestion_huespedes(self):
+        self.controlador.mostrar_frame("GestionHuespedes")
+
+    def mostrar_gestion_reservas(self):
+        self.controlador.mostrar_frame("GestionReservas")
+
+    def cerrar_sesion(self):
+        print("Sesión cerrada exitosamente.")
+        messagebox.showinfo("Cerrar Sesion", "Sesión cerrada exitosamente.")
+        self.controlador.mostrar_frame("Login")
+
+    def volver_menu_encargado(self):
+        self.controlador.mostrar_frame("VentanaEncargado")
+#--------------------------------------------------------- FIN MENU -----------------------------------------------------------------
         
     def cargar_datos(self):
         for item in self.tree.get_children():
